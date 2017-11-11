9a8f8633ab3245c21159d948e20322ebaef9cfe4
Melissa Bower <melissaclausen@boisestate.edu>
2017-11-11 15:36:12 -0700

    Added the landing page things

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,84 +6,60 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
-    <body>
 
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+        <body>
+        <div class="">
+            {{--@if (Route::has('login'))--}}
+            {{--<div class="top-right links">--}}
+            {{--@auth--}}
+            {{--<a href="{{ url('/home') }}">Home</a>--}}
+            {{--@else--}}
+            {{--<a href="{{ route('login') }}">Login</a>--}}
+            {{--<a href="{{ route('register') }}">Register</a>--}}
+            {{--@endauth--}}
+            {{--</div>--}}
+            {{--@endif--}}
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="{{url('http://interfaithsanctuary.org/')}}">
+                            {{--<img alt="Brand" src="...">--}}
+                            <div>Logo</div>
+                        </a>
+
+
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                        <ul class="nav navbar-nav navbar-center">
+
+                            <li><a class="navbar-brand" href="#">Adopt-a-Meal</a></li>
+
+                        </ul>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+
+                            <li><a href="#">Calendar View</a></li>
+
+                            <li><a href="#">Meal Ideas</a></li>
+
+                        </ul>
+
+                    </div><!-- /.navbar-collapse -->
+
                 </div>
 
-                    {{--<a href="https://laravel.com/docs">Documentation</a>--}}
+            </nav>
+
+
         </div>
-
-
-    </body>
-
+        </body>
 </html>
+
+
