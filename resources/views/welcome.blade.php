7dbaf10889610ee06ac1c0b2b5fcc3458e3b3272
mattsmith11 <mattsmith11@u.boisestate.edu>
2017-11-17 10:18:44 -0700

    Feature/demo (#47)
    * Demo complete
    
    * Made it look alright I think
    
    * Updating gitignore with the service_account_creds file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <title>Adopt-A-Meal</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.css' />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.2/moment.min.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.7.0/fullcalendar.min.js'></script>
+        
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     </head>
 
         <body>
@@ -28,7 +33,6 @@
                         <a class="navbar-left" href="{{url('http://interfaithsanctuary.org/')}}">
                             <img class="brand" alt="Brand" src="images/Interfaith-Temp-Logo.png">
                         </a>
-
 
                     </div>
                     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -54,14 +58,15 @@
 
             </nav>
             <div class="container">
-                <div class="col-md-8 col-md-offset-2 calender">
-                    Place Calender Here
-                    @foreach($events as $event)
+                <div class="col-md-10 col-md-offset-2 calendar">
+                    <div id="calendar"></div>
+
+                    <!-- @foreach($events as $event)
 
                         <div>{{$event->start->dateTime}}</div>
 
 
-                    @endforeach
+                    @endforeach -->
                 </div>
 
             </nav>
@@ -80,6 +85,20 @@
 
         </div>
         </body>
+        <script>
+            $(document).ready(function() {
+                 // page is now ready, initialize the calendar...
+                 
+                $('#calendar').fullCalendar({
+                    showNonCurrentDates: false,
+                    contentHeight : "auto",
+                    height: 'parent' +80 ,
+                    aspectRatio: 1.5,
+                    themeSystem: 'bootstrap3'
+                // put your options and callbacks here
+                });
+            });
+        </script>
 </html>
 
 
