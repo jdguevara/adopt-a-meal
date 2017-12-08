3666c5823a180e13f97db83eb5faf1460ff9efff
Melissa Bower <melissaclausen@boisestate.edu>
2017-12-07 18:12:02 -0700

    Added a list item, to the admin view.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><h3>Admin Dashboard</h3></div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -13,10 +13,14 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    Accept, Edit and Delete Adopt-a-Meal requests here.
                 </div>
+
             </div>
+            <ul class="list-group">
+                <li class="list-group-item "><div class="row"><div class= "col-sm-8"><h5>yo ho</h5></div><div class="btn-toolbar col-sm-4"><button type="button" class="btn btn-primary pull-right ">Edit</button> <button type="button" class="btn btn-warning pull-right ">Delete</button></div></div></li>
+            </ul>
+
         </div>
     </div>
 </div>
