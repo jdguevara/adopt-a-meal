1b03c1f3a119d34eef08c72e598fcda551fa088b
Matt Smith <mattsmith11@u.boisestate.edu>
2017-12-09 12:27:11 -0700

    Testing the email service off campus

--- resources/views/emails/volunteerformemail.php
+++ resources/views/emails/volunteerformemail.php
@@ -1,21 +1,6 @@
 @extends('layouts.app')
 
-@section('scripts')
-    <script>
-      $(document).ready(function() {
-        $('#calendar').fullCalendar({
-          showNonCurrentDates: false,
-          contentHeight : "auto",
-          height: 'parent' +80 ,
-          aspectRatio: 1.5,
-          themeSystem: 'bootstrap3'
-        });
-      });
-    </script>
-
-@endsection
-
 //TODO add link
 @section('content')
-
+{{"test"}}
 @endsection
