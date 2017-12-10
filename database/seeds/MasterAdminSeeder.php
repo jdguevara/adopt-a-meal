f7d09fd256ef0006f607689ba5cfdf237a16786d
Matt Smith <mattsmith11@u.boisestate.edu>
2017-12-09 18:01:03 -0700

    Created a database seeder for the master user. Added the fields needed for it to work to the env template. Re-enabled auth since everyone should be able to seed the database with the master user

---
+++ database/seeds/MasterAdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MasterAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Adds the master user to the User table
+        $user = env('MASTER_USER');
+        $pass = env('MASTER_PASS');
+        $email = env('MASTER_EMAIL');
+
+        if(!(empty($user) || empty($pass) || empty($email))) {
+            DB::table('users')->insert([
+                'name' => $user,
+                'email' => $email,
+                'password' => bcrypt($pass)
+            ]);
+        }
+    }
+}
