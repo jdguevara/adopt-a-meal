52a50333f78a15ae9a1ef7a9e39a6d8ec78c11d7
Matt Smith <mattsmith11@u.boisestate.edu>
2018-01-24 21:23:33 -0700

    Added a foundation for the volunteer form seed. This probably shouldn't be a part of the usual seed method

--- database/seeds/VolunteerFormSeeder.php
+++ database/seeds/VolunteerFormSeeder.php
@@ -12,18 +12,23 @@ class MasterAdminSeeder extends Seeder
     public function run()
     {
         // Adds the master user to the User table
-        $env = env('APP_ENV', '');
+        $env = env('APP_ENV');
         if(empty($env) || $env != 'prod')
             return;
 
         //Scaffolded out the seed but getting the EventId/time might be difficult
         DB::table('volunteer_forms')->insert([
-            'open_event_id' => '',
-            'open_event_id' => '',
-            'open_event_id' => '',
-            'open_event_id' => '',
-            'open_event_id' => '',
-            'open_event_id' => '',
+            'organization_name' => '',
+            'email' => '',
+            'phone' => '',
+            'meal_description' => '',
+            'notes' => '',
+            'food_confirmation' => true, // Must be true 
+            'tableware_confirmation' => false, // Can be false
+            'form_status' => 0, // integer, 0 or 1 or 2
+            'open_event_id' => '', // not sure how to get this field
+            'event_date_time' => '', // not sure how to get this field
+            'confirmed_event_id' => '' // not sure how to get this field
         ]);
     }
 }
