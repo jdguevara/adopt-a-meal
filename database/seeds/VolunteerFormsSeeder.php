e85fd5a940f3455440a85f42960a97d4cff8c194
Matt Smith <mattsmith11@u.boisestate.edu>
2018-01-24 22:52:37 -0700

    Seeding mock volunteer requests using events from the Google Calendar now.

--- database/seeds/VolunteerFormsSeeder.php
+++ database/seeds/VolunteerFormsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Calendar;
 
 class VolunteerFormsSeeder extends Seeder
 {
@@ -16,20 +17,23 @@ class VolunteerFormsSeeder extends Seeder
         if(empty($env) || $env == 'prod')
             return;
 
-        echo $calendar->findAll();
-        //Scaffolded out the seed but getting the EventId/time might be difficult
-        DB::table('volunteer_forms')->insert([
-            'organization_name' => '',
-            'email' => '',
-            'phone' => '',
-            'meal_description' => '',
-            'notes' => '',
-            'food_confirmation' => true, // Must be true 
-            'tableware_confirmation' => false, // Can be false
-            'form_status' => 0, // integer, 0 or 1 or 2
-            'open_event_id' => '', // not sure how to get this field
-            'event_date_time' => '', // not sure how to get this field
-            'confirmed_event_id' => '' // not sure how to get this field
-        ]);
+        $events = $calendar->findAll();
+        $max = count($events) > 5 ? 5 : count($events);        
+        for ($i = 0; $i < $max; $i++) {
+            $event = $events[mt_rand(0, count($events) - 1)];
+            DB::table('volunteer_forms')->insert([
+                'organization_name' => 'Seeded Organization',
+                'email' => 'seed@seed.seed',
+                'phone' => '9998887776',
+                'meal_description' => 'Soylent green is people.',
+                'notes' => 'Seed notes',
+                'food_confirmation' => true, // Must be true 
+                'tableware_confirmation' => false, // Can be false
+                'form_status' => 0, // integer, 0=new or 1=confirmed or 2=rejected
+                'open_event_id' => $event['id'], // not sure how to get this field
+                'event_date_time' => new DateTime($event['start']['dateTime']), // not sure how to get this field
+                'confirmed_event_id' => null // not sure how to get this field
+            ]);
+        }
     }
 }
