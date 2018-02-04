a8ddd914fb56b36d885a3b4a2eb71a857930bbb8
melissaclausen <melissaclausen@u.boisestate.edu>
2018-02-03 22:18:57 -0700

    Made the ui function properlly.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -31,10 +31,14 @@
                                 <h4 class="modal-title"></h4>
                             </div>
                             <div class="modal-body">
-                                <p class="date"></p>
+                                <h5 class="organization"></h5>
+                                <hr/>
                                 <p class="email"></p>
-                                <p class="notes"></p>
-
+                                <p class ="phone"></p>
+                                <p class="mealdescription"></p>
+                                <hr/>
+                                <p class ="notes"></p>
+                                <p class ="tablewareconfirmation"></p>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-info pull-left" v-if="" data-dismiss="modal">Accept</button>
@@ -49,14 +53,24 @@
                 @foreach($forms as $form)
                     <ul  class="list-group">
                         <li class="list-group-item "><div class="row">
-                                <div class= "col-sm-6">
-                                    <h5>{{$form->organization_name}}</h5>
-                                    <h6>Date: {{$form->event_date_time}}</h6>
+                                <div class= "col-sm-8">
+                                    <h5>Adopt-a-Meal Request for: {{$form->event_date_time}}</h5>
+                                    <h6>From: {{$form->organization_name}}</h6>
                                 </div>
-                                <div class="btn-toolbar col-sm-6">
-                                    <button type="button" class="btn btn-danger pull-right "  >Decline</button>
-                                    <button type="button" class="btn btn-warning pull-right" data-toggle="modal" :data-organization="{{json_encode($form['organization_name'],true)}}" :data-organization="{{json_encode($form['organization_name'],true)}}" :data-date="{{json_encode($form['event_date_time'],true)}}" :data-email="{{json_encode($form['email'],true)}}" :data-notes="{{json_encode($form['meal_description'],true)}}" data-target="#myModal">Details</button>
-                                    <button type="button" class="btn btn-info pull-right" >Accept</button>
+
+
+                                <div class="btn-toolbar col-sm-4">
+                                    {{--<button type="button" class="btn btn-danger pull-right "  >Decline</button>--}}
+                                    <button type="button" class="btn btn-warning pull-right" data-toggle="modal"
+                                            :data-mealdescription="{{json_encode($form['meal_description'],true)}}"
+                                            :data-organization="{{json_encode($form['organization_name'],true)}}"
+                                            :data-date="{{json_encode($form['event_date_time'],true)}}"
+                                            :data-email="{{json_encode($form['email'],true)}}"
+                                            :data-notes="{{json_encode($form['notes'],true)}}"
+                                            :data-tablewareconfirmation="{{json_encode($form['tableware_confirmation'],true)}}"
+                                            :data-phone="{{json_encode($form['phone'],true)}}"
+                                            data-target="#myModal">Details</button>
+                                    {{--<button type="button" class="btn btn-info pull-right" >Accept</button>--}}
 
                                 </div>
                             </div>
@@ -76,16 +90,33 @@
         $(document).ready(function(){
             $('#myModal').on('show.bs.modal',function(event) {
                 var button = $(event.relatedTarget)
+                var mealDescription = button.data('mealdescription')
                 var organization = button.data('organization')
                 var email = button.data('email')
                 var notes = button.data('notes')
+                var phone = button.data('phone')
                 var date = button.data('date')
 
+                var tablewareConfirmation = button.data('tablewareconfirmation')
+                console.log(tablewareConfirmation)
+                var bringtableware = "They will provide the tableware"
+
+
+
                 var modal = $(this)
-                modal.find('.modal-title').text(organization)
-                modal.find('.email').text("Email: " + email)
-                modal.find('.notes').text("Notes: " + notes)
-                modal.find('.date').text("Date: "+ date)
+                modal.find('.organization').text(organization)
+                if(email)
+                    modal.find('.email').text("Email: " + email)
+                if(phone)
+                    modal.find('.phone').text("Phone: "+ phone)
+                if(mealDescription)
+                  modal.find('.mealdescription').text("Meal Description: " + mealDescription)
+                modal.find('.modal-title').text("Adopt-a-Meal request for: "+ date)
+                if(notes)
+                    modal.find('.notes').text("Notes: "+ notes)
+                if(tablewareConfirmation == '1')
+                    modal.find('.tablewareconfirmation').text("Other Details: They are planning to provide tableware")
+
             })
 
         });
