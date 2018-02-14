7b8dbf78eb351548647572946a4cba84ae730156
Matt Smith <mattsmith11@u.boisestate.edu>
2018-02-14 00:29:55 -0700

    More wording tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -189,12 +189,12 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h3 id="title" style="margin-top: 15px;">This event is already confirmed</h3>
+                        <h3 id="title" style="margin-top: 15px;">An organization has adopted this meal!</h3>
                     </div>  
                     <div class="modal-body">
                         <div id="inputs" class="volunteer-inputs">
                             <div class="input-group">
-                                <span class="input-group-addon">Event Date</span>
+                                <span class="input-group-addon">Date</span>
                                 <input id="confirmed-event-date" name="event_date" type="text"  
                                         class="form-control" disabled>
                             </div>
