bee5fd6843eeeb58574a8164f3a7b8070a019345
Daniel Bakyono <bakyonoognomo@gmail.com>
2018-02-26 19:57:51 -0700

    Adding dusk integration tests. #134

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+
+        $this->browse(function ($browser) {
+            $username = env('MASTER_USER', '');
+            $password = env('MASTER_PASS', '');
+
+            $browser->visit('/login')
+                ->type('email', $username)
+                ->type('password', $password)
+                ->press('Login')
+                ->assertPathIs('/');
+        });
+    }
+}
