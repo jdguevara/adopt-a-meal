2de49027d46afa6127311b096f312118d80d8fce
Matt Smith <mattsmith11@u.boisestate.edu>
2018-02-26 21:37:20 -0700

    Tweaked the meal-idea display

--- resources/views/mealideas.blade.php
+++ resources/views/mealideas.blade.php
@@ -55,7 +55,7 @@ $(document).on('click', '.btn_remove', function(){
 </div>
 <div class="container">
     @foreach ($mealideas as $mealidea)
-    <div class="col-12 col-md-6 col-lg-3">
+    <div class="col-12 col-md-6 col-lg-4">
         <div class="panel panel-primary">
             <div class="panel-heading">{{$mealidea->title}}</div>
             <div class="panel-body">
