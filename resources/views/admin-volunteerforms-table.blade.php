d1a3054563b40b68febca1d56ad5200bf3622ba3
mattsmith11 <mattsmith11@u.boisestate.edu>
2018-03-12 08:12:45 -0600

    Feature/matt/mealidea input order (#216)
    * Changed the order of the inputs for the instructions and ingredients fields on the meal idea modal. Changed the mealidea table view to be contained in a row
    
    * Added a label for the instructions input group

--- resources/views/admin-volunteerforms-table.blade.php
+++ resources/views/admin-volunteerforms-table.blade.php
@@ -14,8 +14,8 @@ $(document).ready(function () {
 
 
 @section('content')
-<div class="container">
-    <div class="row">
+<div class="row">
+    <div class="col-xs-12 col-md-8 col-md-offset-2">
         <div class="panel panel-primary">
             <div class="panel-heading text-center text-capitalize"><h3>Past Volunteer Requests</h3></div>
             <div class="panel-body text-center">
